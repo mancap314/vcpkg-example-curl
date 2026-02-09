1894b8a31279a9b3d04860f134cea28c4300c256
Manuel Capel <manuel.capel82@gmail.com>
2026-02-09 20:03:50 +0100

    lexbor added + example finished

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
+#include <lexbor/core/types.h>
+#include <lexbor/dom/interface.h>
+#include <lexbor/html/interfaces/document.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <curl/curl.h>
+#include <lexbor/html/html.h>
+#include <lexbor/html/interface.h>
+
+#define TITLE_BUFFER_LEN 256
 
 struct MemoryStruct {
   char *memory;
@@ -25,6 +32,67 @@ static size_t mem_cb(void *contents, size_t size, size_t nmemb, void *userp) {
   mem->memory[mem->size] = '\0';
 
   return realsize;
+}
+
+typedef enum {
+  FT_SUCCESS = 0,
+  FT_CREATE_FAILED,
+  FT_PARSING_FAILED,
+  FT_NO_TITLE,
+  FT_N_FETCH_TITLE_STATUS
+} fetch_title_status;
+
+const char fetch_title_message[FT_N_FETCH_TITLE_STATUS][30] = {
+    [FT_SUCCESS] = "OK",
+    [FT_CREATE_FAILED] = "Failed to create document",
+    [FT_PARSING_FAILED] = "Failed to parse document",
+    [FT_NO_TITLE] = "No title in document"};
+
+fetch_title_status fetch_title(lxb_char_t *html, size_t html_len,
+                               lxb_char_t *title_buffer,
+                               size_t title_buffer_len) {
+  lxb_status_t status;
+  lxb_html_document_t *document;
+  const lxb_char_t *title;
+  size_t title_len;
+  fetch_title_status ret = FT_SUCCESS;
+
+  /* initialization */
+  document = lxb_html_document_create();
+  if (NULL == document) {
+    ret = FT_CREATE_FAILED;
+    goto defer_err;
+  }
+
+  /* parse html */
+  status = lxb_html_document_parse(document, html, html_len);
+  if (status != LXB_STATUS_OK) {
+    ret = FT_PARSING_FAILED;
+    goto defer_err;
+  }
+
+  title = lxb_html_document_title(document, &title_len);
+  if (NULL == title) {
+    ret = FT_NO_TITLE;
+    goto defer_err;
+  }
+
+  if (title_len > title_buffer_len - 1) {
+    printf("[WARNING] %s(): title has length %zu, will be truncated to length "
+           "%zu\n",
+           __func__, title_len, title_buffer_len);
+    title_len = title_buffer_len - 1;
+  }
+  memcpy(title_buffer, (const lxb_char_t *)title, title_len);
+  title_buffer[title_len] = '\0';
+
+  lxb_html_document_destroy(document);
+  return ret;
+
+defer_err:
+  if (document)
+    lxb_html_document_destroy(document);
+  return ret;
 }
 
 int main(void) {
@@ -82,10 +150,21 @@ int main(void) {
 
   /* cleanup curl stuff */
   curl_easy_cleanup(curl_handle);
-  free(chunk.memory);
 
   /* we are done with libcurl, so clean it up */
   curl_global_cleanup();
 
-  return EXIT_SUCCESS;
+  int ret = EXIT_SUCCESS;
+  lxb_char_t title_buffer[TITLE_BUFFER_LEN];
+  fetch_title_status fts = fetch_title((lxb_char_t *)chunk.memory, chunk.size,
+                                       title_buffer, TITLE_BUFFER_LEN);
+  if (fts != FT_SUCCESS) {
+    fputs(fetch_title_message[fts], stderr);
+    ret = EXIT_FAILURE;
+  } else {
+    printf("Title found: %s\n", title_buffer);
+  }
+
+  free(chunk.memory);
+  return ret;
 }
